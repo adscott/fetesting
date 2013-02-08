32286f8092e64895b77d2b4bb3eca185acf915f0
Adam Scott <adam.d.scott@gmail.com>
2013-02-08 14:42:56 +1100

    Basic server side services

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'sinatra/json'
+
+dictionary = File.readlines('dictionary.txt').map(&:strip).sort
 
 get '/' do
   erb :index
+end
+
+get '/autocomplete' do
+  json dictionary.select { |word| word.start_with? params[:term] }[0...7]
+end
+
+get '/search' do
+  erb :search
 end
